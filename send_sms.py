ba2c9c71a289e81ab4484de4b8b3935fdccd58c5
Ekaterina Kalache <ekaterinakalache@Ekaterinas-MacBook-Air.local>
2022-10-10 18:41:28 -0400

    Twilio message

---
+++ send_sms.py
@@ -0,0 +1,18 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+# Find your Account SID and Auth Token in Account Info
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    body='Hi there',
+    from_='+19259401304',
+    to='+14153595829'
+    )
+
+print(message.sid)
+                
